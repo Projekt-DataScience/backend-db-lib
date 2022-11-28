0521d2f7dc4442c3ab5b3fe2d12ac3c4f5c232ba
Fabian Hueber <fabi_boy@hotmail.de>
2022-11-28 17:12:21 +0100

    refactored project structure

--- backend-db-lib/manager.py
+++ backend-db-lib/manager.py
@@ -1,6 +1,6 @@
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from .models import base, Company, User, Role, Layer, Group
+from .models import Company, User, Role, Layer, Group
 
 
 class DatabaseManager:
