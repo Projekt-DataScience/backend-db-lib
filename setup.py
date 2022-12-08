cf4ed35bb898d6be4ea1256dedfe8e1eb5af03b3
Korkmatik <fahri.korkmaz@protonmail.ch>
2022-12-08 22:28:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='backend_db_lib',
-    packages=find_packages("backend_db_lib"),
+    packages=[ "backend_db_lib", "backend_db_lib.tests" ],
     version='0.1.0',
     description='Backend Database Library',
     author='Backend-Gang',
