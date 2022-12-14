7167540bb5bb339fcf0151567e06fb5a8bcc6122
Fahri Korkmaz <fahri.korkmaz@protonmail.ch>
2022-12-14 21:18:30 +0100

    Add tests that checks if data created when initialized

---
+++ backend_db_lib/tests/test_manager.py
@@ -0,0 +1,42 @@
+import unittest
+from backend_db_lib.models import (
+    base, User, Company, Role, Group,
+    Layer, LPAQuestionCategory, LPAQuestion,
+    LPAAnswerReason, LPAAudit, AuditQuestionAssociation,
+    LPAAuditDuration
+    )
+from backend_db_lib.manager import DatabaseManager
+
+
+class TestDatabaseManager(unittest.TestCase):
+
+    def setUp(self):
+        self.db = DatabaseManager(base, "sqlite:///test.db")        
+        self.db.create_all()
+
+    def tearDown(self) -> None:
+        self.db.drop_all()
+
+    def test_database_intialize(self):
+        self.db.create_initial_data()
+
+        models = [
+            User, Company, Role, Group, Layer,
+            LPAQuestionCategory, LPAQuestion, 
+            LPAAnswerReason, LPAAudit, AuditQuestionAssociation, 
+            LPAAuditDuration
+        ]
+
+        with self.db.create_session() as session:
+            for model in models:
+                self.__test_init(session, model)
+
+    def __test_init(self, session, model):
+        data = session.query(model).all()
+        self.assertGreater(len(data), 0)
+
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
